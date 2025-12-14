5954312765d296860f90535833eb6ff53a0bbed5
Arthur Rocha Lima <arthurrochalimawork@gmail.com>
2025-12-13 23:39:13 -0300

    Add files via upload

---
+++ Name_cases.py
@@ -0,0 +1,3 @@
+Frase="Deixem que o futuro diga a verdade e avalie cada um de acordo com o seu trabalho e realizações. O presente pertence a eles, mas o futuro pelo qual eu sempre trabalhei pertence a mim."
+autor="nikola tesla"
+print(f"{autor.title()} disse uma vez: \n\"{Frase}\"")
