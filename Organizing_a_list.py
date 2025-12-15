e271121095004cb31d3a2e9bf53779ab94d5a195
Arthur Rocha Lima <arthurrochalimawork@gmail.com>
2025-12-15 17:43:17 -0300

    Add files via upload

---
+++ Organizing_a_list.py
@@ -0,0 +1,11 @@
+#Organizando uma lista
+#4.1 Pizzas
+pizzas=["chocolate","peperoni","quatro queijos"]
+for pizzas in pizzas:
+    print(f"Gosto de pizza de {pizzas.title()}, como eu amo pizza!")
+
+#4.2 Animais
+animais=["cachorro","macaco","cavalo"]
+for animais in animais:
+    print(f"Um{animais.title()} seria um ótimo animal de estimação.")
+print("Todos esses animais seriam ótimos animais de estimação!")
